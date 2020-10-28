9d3dac04e043b893de5c815b5048e20043e3c1f4
shermosa <shermosa@hotels.com>
2020-10-28 15:31:53 +0000

    add ParserLocusTest class

---
+++ src/test/java/com/hotels/mutantswarm/plan/gene/ParserLocusTest.java
@@ -0,0 +1,38 @@
+package com.hotels.mutantswarm.plan.gene;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class ParserLocusTest {
+  
+  private ParserLocus parserLocus;
+  private ParserLocus parserLocus2;
+
+  @Test
+  public void equalsSame() {
+    parserLocus = new ParserLocus(0,2,4);
+    parserLocus2 = new ParserLocus(0,2,4);
+    boolean result = parserLocus.equals(parserLocus2);
+    assertTrue(result);
+  }
+  
+  @Test
+  public void checkGetNodeIndex() {
+    parserLocus = new ParserLocus(0,2,4);
+    int result = parserLocus.getNodeIndex();
+    assertEquals(result,4);
+  }
+  
+  @Test
+  public void checkToString() {
+    parserLocus = new ParserLocus(0,2,4);
+    String result = parserLocus.toString();
+    System.out.println(result);
+    assertEquals(result,"ParserLocus [scriptIndex=0, statementIndex=2, nodeIndex=4]");
+  }
+}
